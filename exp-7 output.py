7ec150522276a09ab0c531013d1b10792c823b8b
192511352simats-ctrl <192511352.simats@saveetha.com>
2026-07-27 11:37:03 +0530

    Delete exp-7 output.py

--- exp-7 output.py
+++
@@ -1,7 +0,0 @@
-Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> 
-=========================== RESTART: D:/DAA/exp-7.py ===========================
-Enter number of elements: 6
-Enter elements: 10 7 8 9 1 5
-Sorted array: [7, 8, 1, 5, 9, 10]
